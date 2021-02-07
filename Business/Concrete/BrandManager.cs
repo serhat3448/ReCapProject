fd3bbe47e57f61fd4a0b2255426b8e28dc5f3bde
serhat doğru <dogru.serhat@hotmail.com>
2021-02-07 18:50:35 +0300

    Ödev 8. Hafta

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public void Add(Brand brand)
+        {
+            if(brand.Name.Length > 2)
+            {
+                _brandDal.Add(brand);
+                Console.WriteLine("Marka başarıyla eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen marka isminin uzunluğunu 2 karakterden fazla giriniz.");
+            }
+        }
+
+        public void Delete(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Brand brand)
+        {
+            if (brand.Name.Length > 2)
+            {
+                _brandDal.Add(brand);
+                Console.WriteLine("Marka başarıyla eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen marka isminin uzunluğunu 2 karakterden fazla giriniz.");
+            }
+        }
+    }
+}
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,50 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araba başarıyla eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen günlük fiyat kısmını 0'dan büyük giriniz.");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(P => P.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            if (car.DailyPrice > 0)
+            {
+                _carDal.Update(car);
+                Console.WriteLine("Araba başarıyla güncellendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen günlük fiyat kısmını 0'dan büyük giriniz.");
+            }
         }
     }
 }
