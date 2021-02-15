bfe9d3be8b68377128762ba5c3d9766afb37a60a
serhat doğru <dogru.serhat@hotmail.com>
2021-02-15 03:05:36 +0300

    10. Gün Ödev Güncellemesi

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,13 +1,43 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, ReCapProjectDbContext>, IRentalDal
     {
+        private Func<Rental, bool> filter;
+
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                var result = from rentals in filter == null ? context.Rentals : context.Rentals.Where(filter)
+                             join cars in context.Cars on rentals.CarId equals cars.Id
+                             join colors in context.Colors on cars.ColorId equals colors.Id
+                             join brands in context.Brands on cars.BrandId equals brands.Id
+                             join customers in context.Customers on rentals.CustomerId equals customers.Id
+                             join users in context.Users on customers.UserId equals users.Id
+                             select new RentalDetailDto
+                             {
+                                 RentalId = rentals.Id,
+                                 FirstName = users.FirstName,
+                                 LastName = users.LastName,
+                                 CompanyName = customers.CompanyName,
+                                 CarName = cars.Description,
+                                 ColorName = colors.Name,
+                                 BrandName = brands.Name,
+                                 RentDate = rentals.RentDate,
+                                 ReturnDate = rentals.ReturnDate
+                             };
+
+                return result.ToList();
+            }       
+        }
     }
 }
---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto
+    {
+        public int RentalId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
